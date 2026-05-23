2ddc38cecada2a2634b7baadcef04e091b35ed7a
Sarkar <sarkarrohit650@gmail.com>
2026-05-23 21:00:20 +0530

    updated the cors config for production url

--- librarymanagement/src/main/java/com/example/librarymanagement/config/CorsConfig.java
+++ librarymanagement/src/main/java/com/example/librarymanagement/config/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200",
+                        "https://library-management-system-vert-eight.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*");
     }
